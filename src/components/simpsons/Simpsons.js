e903fd65996490da8d716a41a1d688508ae97d45
m3dey <misteryakk1523@gmail.com>
2023-04-13 16:28:51 +0300

    HW1

--- src/components/simpsons/Simpsons.js
+++ src/components/simpsons/Simpsons.js
@@ -1,19 +1,2 @@
 import React from 'react';
 
-function Simpsons(props) {
-    let  simpsons=props.props;
-    return (
-        <div>
-            {simpsons.map(value => (
-                <div>
-                    <h3>Name: {value.name}, Surname: {value.surname}</h3>
-                    <p> Age: {value.age}</p>
-                    <img src={value.photo} alt={value.name}/>
-                    <p>{value.info}</p>
-                </div>
-            ))}
-        </div>
-    );
-}
-
-export default Simpsons;
